ad01928d44c7ae8cd3769479b1c6f11df8ee08eb
Yogesh Kumar <yogeshlodhi1208@gmail.com>
2024-03-16 12:13:50 +0530

    Added Panels

---
+++ StudentsPanel/src/App.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <Box className="bg-red-500">
+      <Heading>Yogesh Kumar</Heading>
+    </Box>
+  )
+}
+
+export default App
